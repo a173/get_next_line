bc742abb2df8856b3f0f81ca18cdc2471e88cd4c
Allegra Caryn <acaryn@ev-f1.kzn.21-school.ru>
2021-01-18 18:27:41 +0300

    v1

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acaryn <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/06 19:38:13 by acaryn            #+#    #+#             */
+/*   Updated: 2020/11/12 16:34:55 by acaryn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *str, int ch);
+char	*ft_strcpy(char *dest, char *src);
+int		get_next_line(int fd, char **line);
+
+#endif
